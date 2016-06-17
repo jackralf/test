49c4658c2fc99df8e13f6a1343ba24fd598bf891
machenfei <machenfei1234@gmail.com>
2016-06-18 07:28:36 +0800

    test project

---
+++ STLTest/STLTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STLTest
+//
+//  Created by machenfei on 16/6/18.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
